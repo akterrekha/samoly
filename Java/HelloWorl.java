ff834dc123220d4879b3a8528adfaa4ccedbeadb
unknown <akterrekha>
2018-11-26 03:20:28 +0600

    java

---
+++ Java/HelloWorl.java
@@ -0,0 +1,11 @@
+public class HelloWorl{
+public static  String msg;
+public static  String name="Rekha";
+public static  void main (String[] args){
+msg="hello World!!!!";
+System.out.println("Message: "+msg);
+System.out.println("Name: "+name);
+
+}
+
+}
---
+++ Java/WelcomeWithThreeMessages.java
@@ -0,0 +1,10 @@
+public class WelcomeWithTreeMessages{
+public static void main(String[] args){
+System.out.println("programming is fun!");
+System.out.println("Fundamentals First");
+System.out.println("Problem Driven");
+
+
+}
+
+}
